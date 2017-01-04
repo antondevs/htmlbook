1cb7f7cbaeda3df2e7a2f2a4878dc93cdadfb22f
Anton <anton.nadtoka@yandex.ru>
2017-01-05 00:19:34 +0300

    Implemented loading pages and actions

--- HtmlBook/ViewController.swift
+++ HtmlBook/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    func loadPage(name:String) {
+        let filePath = name.replacingOccurrences(of: "#/", with: "Data/")
+        let path = Bundle.main.path(forResource: filePath, ofType: "html")
+        let url = NSURL(fileURLWithPath: path!)
+        
+        do {
+            let htmlString = try NSString(contentsOfFile: path!, encoding: String.Encoding.utf8.rawValue)
+            webView.loadHTMLString(htmlString as String, baseURL: url.deletingLastPathComponent)
+        } catch {
+            print("Error loading page \(name) ...");
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.webView.delegate = self
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadPage(name: "#/index")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func onAction(action:String) {
+        
+        if (action == "#action/test") {
+            let alert = UIAlertController(title: "HtmlBook", message: "Native Dialog", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
     }
 
-
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        if let pageUrl = request.url {
+            let path = pageUrl.absoluteString
+            
+            if let range = path.range(of: "#/") {
+                let pageName = path.substring(from: range.lowerBound)
+                self.loadPage(name: pageName)
+                return false;
+            }
+            
+            if let range = path.range(of: "#action/") {
+                let actionName = path.substring(from: range.lowerBound)
+                onAction(action: actionName)
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # htmlbook
 Html Book
+
+Simple HtmlBook with Swift 3. App based on WebView and custom native action.
